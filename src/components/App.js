643dc3dcc9f6f5d2734fc43dc9886928cbde5e3f
Mikhail Varushichev <Mikeloangel@gmail.com>
2022-09-25 12:22:05 +0300

    refactor: makes Main component functional

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,12 @@ import Main from './Main';
 import Footer from './Footer';
 
 import api from "../utils/Api";
+import { currentUserContext } from '../contexts/currentUserContext';
 
 import PopupWithForm from './PopupWithForm';
 import ImagePopup from './ImagePopup';
 
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 function App() {
   //Popups section
@@ -19,14 +20,14 @@ function App() {
   const [currentUser, setCurrentUser] = useState(undefined);
 
   //On mount effects
-  React.useEffect(()=>{
+  React.useEffect(() => {
     //retrieve currentUser
     api.getUserMe()
       .then(userMe => {
         setCurrentUser(userMe);
       })
       .catch(err => api.handleError(err));
-  },[])
+  }, [])
 
   function handleEditAvatarClick() {
     setIsEditAvatarPopupOpen(!isEditAvatarPopupOpen);
@@ -52,62 +53,63 @@ function App() {
   }
 
   return (
-    <div className="root">
+    <currentUserContext.Provider value={currentUser}>
+      <div className="root">
 
-      <Header />
-      <Main
-        onEditProfile={handleEditProfileClick}
-        onAddPlace={handleAddPlaceClick}
-        onEditAvatar={handleEditAvatarClick}
-        handleCardClick={handleCardClick}
-        api={api} />
-      <Footer />
+        <Header />
+        <Main
+          onEditProfile={handleEditProfileClick}
+          onAddPlace={handleAddPlaceClick}
+          onEditAvatar={handleEditAvatarClick}
+          handleCardClick={handleCardClick}
+          api={api} />
+        <Footer />
 
-      <PopupWithForm name="edituser" title="Редактировать профиль" buttonLabel="Сохранить" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
-        <label className="popup__form-field">
-          <input className="popup__form-input popup__user-name" id="name-input" name="popup__user-name"
-            placeholder="Представься, путешественник" required minLength="2" maxLength="40" />
-          <span className="popup__form-error name-input-error">#</span>
-        </label>
-        <label className="popup__form-field">
-          <input className="popup__form-input popup__user-description" id="description-input"
-            name="popup__user-description" placeholder="Что влечет тебя к движению?" required minLength="2"
-            maxLength="200" />
-          <span className="popup__form-error description-input-error">#</span>
-        </label>
-      </PopupWithForm>
+        <PopupWithForm name="edituser" title="Редактировать профиль" buttonLabel="Сохранить" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
+          <label className="popup__form-field">
+            <input className="popup__form-input popup__user-name" id="name-input" name="popup__user-name"
+              placeholder="Представься, путешественник" required minLength="2" maxLength="40" />
+            <span className="popup__form-error name-input-error">#</span>
+          </label>
+          <label className="popup__form-field">
+            <input className="popup__form-input popup__user-description" id="description-input"
+              name="popup__user-description" placeholder="Что влечет тебя к движению?" required minLength="2"
+              maxLength="200" />
+            <span className="popup__form-error description-input-error">#</span>
+          </label>
+        </PopupWithForm>
 
-      <PopupWithForm name="editavatar" title="Обновить аватар" buttonLabel="Сохранить" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
-        <label className="popup__form-field">
-          <input className="popup__form-input popup__avatar-link" id="avatar-input" name="popup__avatar-link"
-            placeholder="Адресок подскажите?" type="url" required />
-          <span className="popup__form-error avatar-input-error">#</span>
-        </label>
-      </PopupWithForm>
+        <PopupWithForm name="editavatar" title="Обновить аватар" buttonLabel="Сохранить" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
+          <label className="popup__form-field">
+            <input className="popup__form-input popup__avatar-link" id="avatar-input" name="popup__avatar-link"
+              placeholder="Адресок подскажите?" type="url" required />
+            <span className="popup__form-error avatar-input-error">#</span>
+          </label>
+        </PopupWithForm>
 
-      <PopupWithForm name="newplace" title="Новое место" buttonLabel="Добавить" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
-        <label className="popup__form-field">
-          <input className="popup__form-input popup__place-name" id="place-name-input" name="popup__place-name"
-            placeholder="Какие места привлекли?" required minLength="2" maxLength="30" />
-          <span className="popup__form-error place-name-input-error">#</span>
-        </label>
-        <label className="popup__form-field">
-          <input className="popup__form-input popup__place-url" id="place-url-input" name="popup__place-url"
-            placeholder="Адресок подскажите?" type="url" required />
-          <span className="popup__form-error place-url-input-error">#</span>
-        </label>
-      </PopupWithForm>
+        <PopupWithForm name="newplace" title="Новое место" buttonLabel="Добавить" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
+          <label className="popup__form-field">
+            <input className="popup__form-input popup__place-name" id="place-name-input" name="popup__place-name"
+              placeholder="Какие места привлекли?" required minLength="2" maxLength="30" />
+            <span className="popup__form-error place-name-input-error">#</span>
+          </label>
+          <label className="popup__form-field">
+            <input className="popup__form-input popup__place-url" id="place-url-input" name="popup__place-url"
+              placeholder="Адресок подскажите?" type="url" required />
+            <span className="popup__form-error place-url-input-error">#</span>
+          </label>
+        </PopupWithForm>
 
-      <PopupWithForm title="Уверены?" name="confirm" buttonLabel="Удалить" onClose={closeAllPopups}></PopupWithForm>
+        <PopupWithForm title="Уверены?" name="confirm" buttonLabel="Удалить" onClose={closeAllPopups}></PopupWithForm>
 
-      <ImagePopup
-        card={selectedCard}
-        name="viewplace"
-        handleClose={closeAllPopups}
-      />
+        <ImagePopup
+          card={selectedCard}
+          name="viewplace"
+          handleClose={closeAllPopups}
+        />
 
-      {/* Popup with notification shows API errors */}
-      {/* <div className="popup popup_errors">
+        {/* Popup with notification shows API errors */}
+        {/* <div className="popup popup_errors">
       <div className="popup__content">
         <button className="popup__btn-close" title="Закрыть форму" type="button">
           <img alt="Закрыть" className="popup__btn-close-img" src={btnClosePopup} />
@@ -117,7 +119,8 @@ function App() {
       </div>
     </div> */}
 
-    </div>
+      </div>
+    </currentUserContext.Provider>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Card from "./Card";
 
@@ -6,68 +6,62 @@ import defaultUserPic from '../images/upic-blanc.jpg';
 import btnEditUserPic from '../images/btn-edit-user.svg';
 import btnEditUserInfo from '../images/btn-edit.svg';
 
-export default class Main extends React.Component {
-  constructor(props) {
-    super(props);
+function Main(props) {
+  const [userName, setUserName] = React.useState(undefined);
+  const [userDescription, setUserDescription] = React.useState(undefined);
+  const [userAvatar, setUserAvatar] = React.useState(defaultUserPic);
+  const [cards, setCards] = React.useState([]);
 
-    this.state = {
-      userName: undefined,
-      userDescription: undefined,
-      userAvatar: defaultUserPic,
-      cards: []
-    }
-  }
 
-  componentDidMount() {
+  //Mount effect
+  React.useEffect(() => {
     //Loads user info and then initial cards
-    const userPromise = this.props.api.getUserMe();
-    const initialCardsPromise = this.props.api.getInitialCards();
+    const userPromise = props.api.getUserMe();
+    const initialCardsPromise = props.api.getInitialCards();
 
     Promise.all([userPromise, initialCardsPromise])
       .then(list => {
         const [udata, cardList] = list;
+        const {name, about, avatar} = udata;
 
-        this.setState({
-          userName: udata.name,
-          userDescription: udata.about,
-          userAvatar: udata.avatar,
-          cards: cardList
-        })
+        setUserName(name);
+        setUserDescription(about);
+        setUserAvatar(avatar);
+        setCards(cardList);
       })
-      .catch(err => this.props.api.handleError(err))
-  }
+      .catch(err => props.api.handleError(err))
+  }, [])
 
-  render() {
-    return (
-      <main className="main">
+  return (
+    <main className="main">
 
-        <section className="section section-user">
-          <div className="section-user__pic-container">
-            <img alt="User" className="section-user__pic" src={this.state.userAvatar} />
-            <div className="section-user__pic-edit" onClick={this.props.onEditAvatar}>
-              <img src={btnEditUserPic} alt="Сменмить аватарку" className="section-user__btn-edit-pic" />
-            </div>
+      <section className="section section-user">
+        <div className="section-user__pic-container">
+          <img alt="User" className="section-user__pic" src={userAvatar} />
+          <div className="section-user__pic-edit" onClick={props.onEditAvatar}>
+            <img src={btnEditUserPic} alt="Сменмить аватарку" className="section-user__btn-edit-pic" />
           </div>
+        </div>
 
-          <div className="section-user__info">
-            <div className="section-user__container">
-              <h1 className="section-user__name">{this.state.userName ?? 'Recieving...'}</h1>
-              <button className="btn-edit section-user__edit" title="Редактировать профиль" name="section-user__edit-btn" type="button" onClick={this.props.onEditProfile}>
-                <img alt="Редактировать профиль" className="btn-edit__img section-user__edit-btn" src={btnEditUserInfo} />
-              </button>
-            </div>
-            <p className="section-user__description">{this.state.userDescription ?? 'Recieving...'}</p>
+        <div className="section-user__info">
+          <div className="section-user__container">
+            <h1 className="section-user__name">{userName ?? 'Recieving...'}</h1>
+            <button className="btn-edit section-user__edit" title="Редактировать профиль" name="section-user__edit-btn" type="button" onClick={props.onEditProfile}>
+              <img alt="Редактировать профиль" className="btn-edit__img section-user__edit-btn" src={btnEditUserInfo} />
+            </button>
           </div>
-          <button className="section-user__addpost btn-plus" title="Добавить пост" type="button" onClick={this.props.onAddPlace}></button>
-        </section>
+          <p className="section-user__description">{userDescription ?? 'Recieving...'}</p>
+        </div>
+        <button className="section-user__addpost btn-plus" title="Добавить пост" type="button" onClick={props.onAddPlace}></button>
+      </section>
 
-        <section aria-label="Фотографии из путешествий!" className="section section-gallery">
-          <ul className="section-gallery__grid">
-            {this.state.cards.map(card => (<Card card={card} key={card._id} onCardClick={this.props.handleCardClick}/>))}
-          </ul>
-        </section>
-      </main>
-    );
-  }
+      <section aria-label="Фотографии из путешествий!" className="section section-gallery">
+        <ul className="section-gallery__grid">
+          {cards.map(card => (<Card card={card} key={card._id} onCardClick={props.handleCardClick} />))}
+        </ul>
+      </section>
+    </main>
+  );
 }
 
+export default Main;
